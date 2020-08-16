fd889344ee5891364567b4d9a8e501de7013775a
Nishant Ranjan <33540353+nishuranjan@users.noreply.github.com>
2020-08-16 21:44:13 +0530

    Update tweet_EMR.py

--- tweet_EMR.py
+++ tweet_EMR.py
@@ -2,6 +2,7 @@ from __future__ import print_function
 
 import sys
 import json
+import time
 
 from pyspark import SparkContext
 from pyspark.streaming import StreamingContext
@@ -9,28 +10,23 @@ from pyspark.streaming.kinesis import KinesisUtils, InitialPositionInStream
 
 if __name__ == "__main__":
     applicationName = "PythonStreamingKinesisWordCountAsl"
-    streamName = "KinesisDemo"
-    endpointUrl = "https://kinesis.us-east-1.amazonaws.com"
-    regionName = "us-east-1"
+    streamName= "KinesisDemo"
+    endpointUrl="https://kinesis.us-east-1.amazonaws.com"
+    regionName="us-east-1"
     sc = SparkContext(appName=applicationName)
     ssc = StreamingContext(sc, 5)
 
-    print("appname is" + applicationName +
-          streamName + endpointUrl + regionName)
-    lines = KinesisUtils.createStream(
-        ssc, applicationName, streamName, endpointUrl, regionName, InitialPositionInStream.LATEST, 2)
-
-    # lines.pprint()
+    print("appname is" + applicationName + streamName + endpointUrl + regionName)
+    lines = KinesisUtils.createStream(ssc, applicationName, streamName, endpointUrl, regionName, InitialPositionInStream.LATEST, 2)
 
     def filter_tweets(x):
         json_tweet = json.loads(x)
-	if json_tweet.has_key('lang'):  # When the lang key was not present it caused issues
-       	if json_tweet['lang'] == 'ar':
-		return True  # filter() requires a Boolean value
+		if json_tweet.has_key('lang'):
+        	if json_tweet['lang'] == 'ar':
+            	return True
     	return False
 
-    lines.foreachRDD(lambda rdd: rdd.filter(filter_tweets).coalesce(
-        1).saveAsTextFile("./tweets/%f" % time.time()))
+    lines.foreachRDD(lambda rdd: rdd.filter(filter_tweets).coalesce(1).saveAsTextFile("./tweets/%f" % time.time()) )
 
     ssc.start()
     ssc.awaitTermination()
